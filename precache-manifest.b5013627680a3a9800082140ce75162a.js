103ff237a00e200bc43355be03b4c87a66f65457
Pawan Gupta <pawangupta1911@gmail.com>
2023-04-28 15:24:10 +0530

    News App

---
+++ precache-manifest.b5013627680a3a9800082140ce75162a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "382b1bcf18535b90a78f",
+    "url": "/static/css/main.7e5d5db2.chunk.css"
+  },
+  {
+    "revision": "382b1bcf18535b90a78f",
+    "url": "/static/js/main.382b1bcf.chunk.js"
+  },
+  {
+    "revision": "98fae294dabfb7543782",
+    "url": "/static/js/1.98fae294.chunk.js"
+  },
+  {
+    "revision": "6017c1192488e128a242",
+    "url": "/static/js/2.6017c119.chunk.js"
+  },
+  {
+    "revision": "b600c0325db6bc8bf28f",
+    "url": "/static/js/runtime~main.b600c032.js"
+  },
+  {
+    "revision": "2f5244ae6fc6697e3269983d9caf95d5",
+    "url": "/index.html"
+  }
+];
